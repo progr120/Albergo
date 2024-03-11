54d09331afa64e3121c877ebc87e3c59172b4523
Joss <gabriel.dossantos@ciacdidattica.it>
2024-03-11 10:24:58 +0100

    finalAlbergo

--- esempiOOluca/src/Guest.java
+++ esempiOOluca/src/Guest.java
@@ -4,18 +4,10 @@ public class Guest {
      */
     private String firstName;
     private String lastName;
-    private String idNumber;
-    private String docType;
-    private String email;
-    private String phone;
 
     public Guest(String firstName, String lastName, String idNumber, String docType, String email, String phone) {
         this.firstName = firstName;
         this.lastName = lastName;
-        this.idNumber = idNumber;
-        this.docType = docType;
-        this.email = email;
-        this.phone = phone;
     }
 
     public Guest(String firstName, String lastName) {
@@ -37,37 +29,4 @@ public class Guest {
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
-
-    public String getIdNumber() {
-        return idNumber;
-    }
-
-    public void setIdNumber(String idNumber) {
-        this.idNumber = idNumber;
-    }
-
-    public String getDocType() {
-        return docType;
-    }
-
-    public void setDocType(String docType) {
-        this.docType = docType;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
 }
